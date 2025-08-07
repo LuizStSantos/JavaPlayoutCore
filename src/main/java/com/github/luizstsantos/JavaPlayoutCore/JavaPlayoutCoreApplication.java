aeee8e6c5e7b8931188d488ac67b1d90343e6bba
LuizStSantos <luiz.st.santos@gmail.com>
2025-08-07 20:26:45 -0300

    first commit

---
+++ src/main/java/com/github/luizstsantos/JavaPlayoutCore/JavaPlayoutCoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.luizstsantos.JavaPlayoutCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPlayoutCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPlayoutCoreApplication.class, args);
+	}
+
+}
